6cd719fc8386dd87356fe401e41f6d37fa4682a6
Lukerman <103198262+Lukerman@users.noreply.github.com>
2023-10-30 22:52:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,6 @@ CHANNEL_ID = -1001783918221  # Replace with your channel ID
 # Replace 'OWNER_USER_ID' with the user ID of the owner
 OWNER_USER_ID = 1778070005  # Replace with the owner's user ID
 
-# Define a list of allowed user IDs who can generate lottery numbers
-allowed_user_ids = [1778070005, 987654321]  # Replace with your allowed user IDs
-
 # Set up MongoDB connection
 # Replace 'YOUR_CONNECTION_STRING' with your MongoDB Atlas connection string
 connection_string = "mongodb+srv://sujithasatheesan8:ZoA8Pqr0jOaC314V@cluster0.54frnzg.mongodb.net/?retryWrites=true&w=majority"
@@ -68,60 +65,47 @@ def generate_lottery_numbers(message):
         bot.reply_to(message, "Please send your mobile number to register first.")
         return
 
-    if user_id in allowed_user_ids:
-        if user_id in user_lottery_status and user_lottery_status[user_id]:
-            bot.reply_to(message, "Sorry, you have already generated a lottery number.")
-        else:
-            num_digits = 4   # Number of random digits
-            num_letters = 2  # Number of random uppercase letters
-
-            letters = ''.join(random.choice(string.ascii_uppercase) for _ in range(num_letters))
-            digits = ''.join(str(random.randint(0, 9)) for _ in range(num_digits))
-            ticket = f"{letters}-{digits}"
-
-            user_lottery_status[user_id] = True  # Mark the user as having generated a lottery number
-            mobile_number = user_mobile_numbers.get(user_id)  # Retrieve mobile number
-
-            # Send the lottery ticket to the user
-            bot.reply_to(message, f"Your lottery ticket: {ticket}")
-
-            # Store the lottery ticket in MongoDB
-            collection.insert_one({
-                "user_id": user_id,
-                "mobile_number": mobile_number,
-                "ticket": ticket
-            })
-
-            # Update in-memory lottery_tickets list
-            lottery_tickets.append((user_id, mobile_number, ticket))
+    if user_id in user_lottery_status and user_lottery_status[user_id]:
+        bot.reply_to(message, "Sorry, you have already generated a lottery number.")
     else:
-        bot.reply_to(message, "Sorry, you are not authorized to generate lottery numbers.")
+        num_digits = 4   # Number of random digits
+        num_letters = 2  # Number of random uppercase letters
+
+        letters = ''.join(random.choice(string.ascii_uppercase) for _ in range(num_letters))
+        digits = ''.join(str(random.randint(0, 9)) for _ in range(num_digits))
+        ticket = f"{letters}-{digits}"
+
+        user_lottery_status[user_id] = True  # Mark the user as having generated a lottery number
+        mobile_number = user_mobile_numbers.get(user_id)  # Retrieve mobile number
+
+        # Send the lottery ticket to the user
+        bot.reply_to(message, f"Your lottery ticket: {ticket}")
+
+        # Store the lottery ticket in MongoDB
+        collection.insert_one({
+            "user_id": user_id,
+            "mobile_number": mobile_number,
+            "ticket": ticket
+        })
+
+        # Update in-memory lottery_tickets list
+        lottery_tickets.append((user_id, mobile_number, ticket))
 
 @bot.message_handler(commands=['reset'])
 def reset_bot(message):
     user_id = message.from_user.id
 
     if user_id == OWNER_USER_ID:
-        global user_mobile_numbers
-        global user_lottery_status
-        global lottery_tickets
-        global allowed_user_ids  # Add this line to access the list of allowed users
-
-        # Clear user data
-        user_mobile_numbers = {}
-        user_lottery_status = {}
-        lottery_tickets = []
+        # Clear user data and restrictions
+        user_mobile_numbers.clear()
+        user_lottery_status.clear()
 
         # Delete all user documents from the MongoDB collection
-        deleted_count = collection.delete_many({}).deleted_count
+        collection.delete_many({})
 
-        # Clear the list of allowed users
-        allowed_user_ids = []
-
-        bot.reply_to(message, f"Bot has been reset. {deleted_count} user(s) have been removed along with their lottery tickets, and all added users have been deleted.")
+        bot.reply_to(message, "Bot has been reset. All user data and restrictions have been cleared.")
     else:
         bot.reply_to(message, "You are not authorized to reset the bot.")
-
 
 @bot.message_handler(commands=['list'])
 def list_lottery_numbers(message):
@@ -161,23 +145,6 @@ def select_winner(message):
     else:
         bot.reply_to(message, "You are not authorized to select a winner.")
 
-@bot.message_handler(commands=['adduser'])
-def add_user_authorization(message):
-    user_id = message.from_user.id
-
-    if user_id == OWNER_USER_ID:
-        if message.reply_to_message and message.reply_to_message.from_user:
-            authorized_user_id = message.reply_to_message.from_user.id
-            if authorized_user_id not in allowed_user_ids:
-                allowed_user_ids.append(authorized_user_id)
-                collection.update_one({"user_id": authorized_user_id}, {"$set": {"is_authorized": True}}, upsert=True)
-                bot.reply_to(message, f"User with ID {authorized_user_id} has been authorized to use the bot.")
-            else:
-                bot.reply_to(message, f"User with ID {authorized_user_id} is already authorized.")
-        else:
-            bot.reply_to(message, "Please reply to a user's message to authorize them to use the bot.")
-    else:
-        bot.reply_to(message, "You are not authorized to add user authorization.")
-
 if __name__ == '__main__':
     bot.polling()
+        
